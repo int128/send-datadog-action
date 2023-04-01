84cb77adc0d3e35ffba9480e1dce403e1707578b
Hidetake Iwata <int128@gmail.com>
2023-04-01 18:51:29 +0900

    Fix test

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -1,5 +1,22 @@
 import { run } from '../src/run'
+import { v1 } from '@datadog/datadog-api-client'
+
+jest.mock('@datadog/datadog-api-client')
+const metricsApiMock = {
+  submitMetrics: jest.fn<Promise<v1.IntakePayloadAccepted>, [v1.MetricsApiSubmitMetricsRequest]>(),
+}
+const metricsApiConstructor = v1.MetricsApi as jest.Mock
+metricsApiConstructor.mockReturnValue(metricsApiMock)
 
 test('run successfully', async () => {
-  await expect(run({ name: 'foo' })).resolves.toBeUndefined()
+  metricsApiMock.submitMetrics.mockResolvedValue({ status: 'ok' })
+  await expect(
+    run({
+      datadogApiKey: 'DATADOG_API_KEY',
+      metricName: 'my.metric',
+      metricType: 'gauge',
+      metricValue: 100,
+      metricTags: ['key:value'],
+    })
+  ).resolves.toBeUndefined()
 })
